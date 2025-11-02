79984783a339526f9154b377a89a18f268ec8c3d
Hadeer Salah <hadeersalah127@gmail.com>
2025-11-02 12:35:02 +0200

    adding the notFound if a provided meal isn't found

--- app/meals/error.jsx
+++ app/meals/error.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const error = ({ error }) => {
     return (
         <main className="error">
-            <h1>An Error Occured!</h1>
+            <h1>Meal Not Found!</h1>
             <p style={{ color: "#de1010" }}>Failed To Fetch Meal Data. Please Try Again Later.</p>
         </main>
     );
